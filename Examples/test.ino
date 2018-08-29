444b7da323fedd1f6d25c0fe00749353e269a846
BlueArduino20 <bluearduino20@gmail.com>
2018-08-29 16:06:24 +0200

    Update test.ino

--- Examples/test.ino
+++ Examples/test.ino
@@ -3,9 +3,10 @@
 void setup() {
 }
 
-
 void loop() {
   LocaleDigiKeyboard.sendKeyStroke(0);
   LocaleDigiKeyboard.delay(2000);
   LocaleDigiKeyboard.println("!\"#$%&'()*+,-./0123456789:;<=>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^`_abcdefghijklmnopqrstuvwxyz{|}~ @");
+  // Digispark should write "!"#$%&'()*+,-./0123456789:;<=>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^`_abcdefghijklmnopqrstuvwxyz{|}~ @"
+  // If it writes that string correctly, everything is right.
 }
